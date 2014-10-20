c18a1457d99fb8a98e56365946bb6f4b0d57769c
Sam Lau <samlau95@gmail.com>
2014-10-20 01:26:34 -0700

    Sidebar

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ skrollr.menu.init(s, {
     easing: 'sqrt',
 
     //Multiply your data-[offset] values so they match those set in skrollr.init
-    scale: 2,
+    // scale: 2,
 
     //How long the animation should take in ms.
     duration: function(currentTop, targetTop) {
@@ -18,12 +18,6 @@ skrollr.menu.init(s, {
 
         //But you could calculate a value based on the current scroll position (`currentTop`) and the target scroll position (`targetTop`).
         //return Math.abs(currentTop - targetTop) * 10;
-    },
-
-    //If you pass a handleLink function you'll disable `data-menu-top` and `data-menu-offset`.
-    //You are in control where skrollr will scroll to. You get the clicked link as a parameter and are expected to return a number.
-    handleLink: function(link) {
-        return 400;//Hardcoding 400 doesn't make much sense.
     },
 
     //By default skrollr-menu will only react to links whose href attribute contains a hash and nothing more, e.g. `href="#foo"`.
